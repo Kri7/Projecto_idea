ffa44d1f54c9597a8ed35434b7050102553d8ab4
Kristina <chris.komoni@gmail.com>
2023-03-29 10:45:26 +0200

    primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double decimales = 4.2;
+        int edad = 12;
+
+        System.out.println("value");
+    }
+}
