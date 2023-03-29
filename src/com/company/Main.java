440646db4a61924bdeca68e87bc1740e3a2a90c8
Kristina <chris.komoni@gmail.com>
2023-03-29 10:54:09 +0200

    cambie la edad

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         double decimales = 4.2;
-        int edad = 12;
+        int edad = 17;
 
         System.out.println("value");
     }
