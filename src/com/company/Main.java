d3cbf658e22dc272669e8139840ec1db0eb4fce5
Kristina <chris.komoni@gmail.com>
2023-03-29 13:57:26 +0200

    ejercicios

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int a=10;
+        //a++; //a=a+1;
+        //System.out.println("a = " + a);//
+        ++a; a= a+1;
+        System.out.println("a = " + (++a));
+
+        System.out.println("Result: " + 2 + 3 + 5);
+        System.out.println("Result: " + 2 + 3 * 5);
+
+        int b = 4;
+        b-- ;
+        System.out.println (--b);
+        System.out.println(b);
+
 
         double decimales = 4.2;
-        int edad = 17;
-
+        int jjju123 = 17;
         System.out.println("value");
+
+        int first = 2;
+        System.out.println(first); // imprime 2
+        int second = 4;
+        System.out.println(second); // imprime 4
+        int sum = first + second; // asigna la suma a la variable sum
+        System.out.println(sum); // imprime 6
+
+        System.out.println(2 + 2);
+        //System.out.println("Cuatro2: " + 2 + 2);
+
+        System.out.println();
     }
 }
