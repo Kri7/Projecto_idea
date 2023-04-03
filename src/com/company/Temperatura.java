b6b67c9dea63d7417b979037acff48915d6edd96
Kristina <chris.komoni@gmail.com>
2023-04-03 11:54:47 +0200

    ejercicios

--- src/com/company/Temperatura.java
+++ src/com/company/Temperatura.java
@@ -3,16 +3,24 @@ package com.company;
 import java.util.Scanner;
 
 public class Temperatura {
+
     public static void main(String[] args) {
+
         Scanner consola = new Scanner(System.in);
+        int temperatura;
+
         System.out.println("Indique la temperatura");
-        int temperatura = consola.nextInt();
+        temperatura = consola.nextInt();
+
         if (temperatura < 5) {
             System.out.println("¡A esquiar!");
+
         } else if (temperatura > 15 && temperatura < 25) {
             System.out.println("¡A la montaña!");
+
         } else if (temperatura > 25) {
             System.out.println("¡A la playa!");
+
         } else {
             System.out.println("¡A descansar... Zzz!");
         }
