a5a937c1b45682724982ae91ef395a7ce10052eb
Kristina <chris.komoni@gmail.com>
2023-04-03 14:45:36 +0200

    ejercicios

---
+++ src/com/company/Par.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Par {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduce un numero: ");
+        int numero = scan.nextInt();
+
+
+        if (numero % 2 == 0) {
+            System.out.println("numero " + numero + " es par!");
+        } else {
+            System.out.println("numero " + numero + " es impar!");
+        }
+    }
+
+}
